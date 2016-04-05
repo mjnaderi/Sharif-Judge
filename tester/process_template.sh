27af0bcfd2949f65c27d0b05299c39d6b8c06b39
pntruongan <pntruongan@gentoopc>
2016-04-05 11:31:59 +0700

    nothing

--- tester/process_template.sh
+++ tester/process_template.sh
@@ -5,7 +5,7 @@ t=${1}
 f=${2}
 banned=`sed -n -e '/\/\*###Begin banned keyword/,/###End banned keyword/p' $t | sed -e '1d' -e '$d'`
 code=`sed -e '1,/###End banned keyword/d' $t`
-echo "$banned"
+#echo "$banned"
 #echo "$code"
 
 while read -r line
